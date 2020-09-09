ea2d08d5b81364b3af37355767bf9b55b23d8a44
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-09 02:36:26 -0300

    cria view para listar tasks

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
 
@@ -14,3 +16,9 @@ def apiOverview(request):
         'Delete task': '/delete-task/<str:id>/',
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
