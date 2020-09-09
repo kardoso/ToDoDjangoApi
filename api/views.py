cdc00afc22af99b2187daf9586af2ded9123ec6b
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-09 02:39:52 -0300

    cria view para detalhes de uma task específica

--- api/views.py
+++ api/views.py
@@ -22,3 +22,9 @@ def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, id):
+    task = Task.objects.get(id=id)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
