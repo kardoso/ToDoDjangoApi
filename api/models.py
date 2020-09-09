30cab715fe47bbdcb9ff07879ad545fd8f75732a
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-09 01:58:49 -0300

    corrige typo na classe modelo Task

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Taks(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=150)
     completed = models.BooleanField(default=False, blank=True, null=True)
 
