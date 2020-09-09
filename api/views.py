7ed09b42cb3055211e92f4f31b88856b75b9d3f6
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-09 01:55:58 -0300

    cria view da visão geral da api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'Task list': '/task-list/',
+        'Task details': '/task/<str:id>/',
+        'Create task': '/create-task/',
+        'Edit task': '/update-task/<str:id>/',
+        'Delete task': '/delete-task/<str:id>/',
+    }
+    return Response(api_urls)
