fb8d56cc84ff5407cab683f81c0486e3283f95fd
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-09 13:37:16 -0300

    adiciona campo de autor ao modelo da task

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
+    author = models.CharField(max_length=150)
     title = models.CharField(max_length=150)
     completed = models.BooleanField(default=False, blank=True, null=True)
 
